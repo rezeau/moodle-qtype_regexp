1526598fccf3ab2d38217b26e8003100ae66093d
Joseph Rezeau <joseph@rezeau.org>
2012-01-15 16:43:50 +0100

    added restore from 1.9
    Signed-off-by: Joseph Rezeau <joseph@rezeau.org>

---
+++ backup/moodle1/lib.php
@@ -0,0 +1,59 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    qtype
+ * @subpackage regexp
+ * @author     Joseph Rézeau
+ * @copyright  2011 Joseph Rézeau
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * REGEXP question type conversion handler
+ */
+class moodle1_qtype_regexp_handler extends moodle1_qtype_handler {
+
+    /**
+     * @return array
+     */
+    public function get_question_subpaths() {
+        return array(
+            'ANSWERS/ANSWER',
+            'REGEXP',
+        );
+    }
+
+    /**
+     * Appends the regexp specific information to the question
+     */
+    public function process_question(array $data, array $raw) {
+
+        // convert and write the answers first
+        if (isset($data['answers'])) {
+            $this->write_answers($data['answers'], $this->pluginname);
+        }
+
+        // convert and write the regexp extra fields
+        foreach ($data['regexp'] as $regexp) {
+            $regexp['id'] = $this->converter->get_nextid();
+            $this->write_xml('regexp', $regexp, array('/regexp/id'));
+        }
+    }
+}
