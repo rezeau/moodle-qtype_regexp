eeb9ad421374c7fdbfa8537e4b6f887e438b1213
Joseph Rézeau <moodle@rezeau.org>
2012-10-29 16:51:52 +0100

    version 2012102900 : Tidied up moodle 2.3 version before sending to moodle plugins repository
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_regexp';
-$plugin->version  = 2012102400;
+$plugin->version  = 2012102900;
 
 $plugin->requires = 2011070102.03;
 $plugin->dependencies = array(
@@ -34,6 +34,6 @@ $plugin->dependencies = array(
     'qbehaviour_regexpadaptivewithhelpnopenalty'  => 2012022600,
 );
 
-$plugin->maturity  = MATURITY_BETA;
+$plugin->maturity  = MATURITY_STABLE;
 
 
