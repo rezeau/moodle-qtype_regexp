de0ca908162159ddd1a9720c6a528ec33d58fa7f
Joseph Rézeau <moodle@rezeau.org>
2012-11-06 18:03:51 +0100

    added header info to lang/en/qtype_regexp.php
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- lang/en/qtype_regexp.php
+++ lang/en/qtype_regexp.php
@@ -1,4 +1,30 @@
 <?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_regexp', language 'en', branch 'MOODLE_23_STABLE'
+*
+* @package   qtype_regexp
+* @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+defined('MOODLE_INTERNAL') || die();
+
 $string['addahint'] = 'Add a hint';
 $string['addmoreanswers'] = 'Add {no} more answer';
 $string['answer'] = 'Answer: ';
@@ -70,4 +96,3 @@ In <strong>Adaptive No penalty</strong> mode the button displayed will say "<str
 
 By default Help button mode value is set at <b>None</b>';
 $string['word'] = 'Word';
-?>
