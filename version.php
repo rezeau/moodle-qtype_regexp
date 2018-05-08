01a1bc468cffe359cbb2292d46566f5cf5f7b9c6
Nicolas Dunand <nico@kolya.ch>
2018-05-08 08:41:05 +0200

    Bumped version number for Moodle 3.5 release

--- version.php
+++ version.php
@@ -27,10 +27,10 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_regexp';
 $plugin->dependencies = array(
-    'qbehaviour_regexpadaptivewithhelp'   => 2017050100,
-    'qbehaviour_regexpadaptivewithhelpnopenalty'  => 2017050100,
+    'qbehaviour_regexpadaptivewithhelp'   => 2018050800,
+    'qbehaviour_regexpadaptivewithhelpnopenalty'  => 2018050800,
 );
-$plugin->version  = 2017050100;
+$plugin->version  = 2018050800;
 $plugin->requires = 2015051100; // Moodle version.
-$plugin->release = '3.3.0 for Moodle 2.9-3.3';
+$plugin->release = '3.5.0 for Moodle 2.9-3.5';
 $plugin->maturity  = MATURITY_STABLE;
